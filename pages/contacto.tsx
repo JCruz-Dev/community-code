eae80366e98c7610ace2223249abeaeb0ae5a41c
Willishakespeare <Skr13@outlook.com>
2020-11-23 13:24:15 -0700

    config repo

--- pages/contacto.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/Layout'
-
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Contacto</h1>
-  </Layout>
-)
-
-export default IndexPage
