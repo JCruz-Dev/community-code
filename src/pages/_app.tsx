dde9e103041a054403b84470c67c1ed562041c55
Juan carlos <juancarloscruz278@gmail.com>
2020-12-22 00:07:58 -0400

    feat: setup React query on root element.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,20 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@emotion/react';
 import GlobalStyles from '@Styles/globalStyled';
 import theme from '@Styles/theme';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Hydrate } from 'react-query/hydration';
 
+const queryClient = new QueryClient();
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 };
 
