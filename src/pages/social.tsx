5c0e375eb242d3457dc4017e6160b43a17ca5da5
Juan carlos <juancarloscruz278@gmail.com>
2020-12-22 21:24:46 -0400

    fix: removes not used files.

--- src/pages/social.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/Layout';
-
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Social</h1>
-  </Layout>
-);
-
-export default IndexPage;
