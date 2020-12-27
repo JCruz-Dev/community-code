ee9c9c186f1799add291d8558a6be06636f8a8c6
Juan carlos <juancarloscruz278@gmail.com>
2020-12-27 18:12:58 -0400

    remove: delete unused files.

--- src/pages/contenido.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/Layout';
-
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Contenido</h1>
-  </Layout>
-);
-
-export default IndexPage;
